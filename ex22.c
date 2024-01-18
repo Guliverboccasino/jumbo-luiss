71781e65e68d387d24b2c4cd50e259f857ca62ed
Guliverboccasino <156683529+Guliverboccasino@users.noreply.github.com>
2024-01-18 14:49:39 +0100

    Add files via upload

---
+++ ex22.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    char parola[14]={'G','i','g','i',32,'D',39,'A','l','e','s','s','i','o'};
+    printf("Il mio cantante preferito: %s \n", parola);
+    return 0;
+}
